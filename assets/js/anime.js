c9ed395098798a86d2e8c925f2f9f41e7a1b2eec
Rodrigo TUTZ <rodrigoantunestutz@gmail.com>
2022-04-15 01:59:02 -0300

    Animação Saudação

---
+++ assets/js/anime.js
@@ -0,0 +1,26 @@
+var $targetItem= $(".anime-item")
+var $targetImg = $(".anime-img")
+var animatioClass = "anime-start"
+var offset = $(window).height()
+function animeItem(){
+    var documentTop = $(document).scrollTop()
+    $targetItem.each(function() {
+        var itemTop = $targetItem.offset().top
+        if(documentTop > itemTop - 200) {
+            $targetItem.addClass(animatioClass)
+        }
+    })
+}
+
+function animeImg(){
+    var documentTop = $(document).scrollTop()
+    $targetImg.each(function() {
+        var itemTop = $targetImg.offset().top
+        if(documentTop > itemTop - 200) {
+            $targetImg.addClass(animatioClass)
+        }
+    })
+}
+
+animeItem();
+animeImg();
