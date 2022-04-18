6d2dc9899c0411826b7ba1c0e93cf255dc8319ef
Rodrigo TUTZ <rodrigoantunestutz@gmail.com>
2022-04-18 01:50:51 -0300

    Skills

--- assets/js/anime.js
+++ assets/js/anime.js
@@ -1,25 +1,81 @@
 var $targetItem= $(".anime-item")
 var $targetImg = $(".anime-img")
-var animatioClass = "anime-start"
+var targetAbout = $(".sobre-content")
+var card1 = $(".card-item-1")
+var card2 = $(".card-item-2")
+var card3 = $(".card-item-3")
+
+var animationClass = "anime-start"
+
 var offset = $(window).height()
+
 function animeItem(){
     var documentTop = $(document).scrollTop()
-    $targetItem.each(function() {
-        var itemTop = $targetItem.offset().top
-        if(documentTop > itemTop - 200) {
-            $targetItem.addClass(animatioClass)
-        }
-    })
+    var itemTop = $targetItem.offset().top
+    if(documentTop > itemTop - 200) {
+        $targetItem.addClass(animationClass)
+    }
 }
 
 function animeImg(){
     var documentTop = $(document).scrollTop()
-    $targetImg.each(function() {
-        var itemTop = $targetImg.offset().top
-        if(documentTop > itemTop - 200) {
-            $targetImg.addClass(animatioClass)
+    var itemTop = $targetImg.offset().top
+    if(documentTop > itemTop - 200) {
+        $targetImg.addClass(animationClass)
+    }
+}
+
+function animeAbout(){
+    var documentTop = $(document).scrollTop()
+
+    targetAbout.each(function(){
+        var itemTop = targetAbout.offset().top
+        if(documentTop > itemTop - 300) {
+            targetAbout.addClass(animationClass)
+        }
+    })
+    
+}
+
+function animeCard1(){
+    var documentTop = $(document).scrollTop()
+
+    card1.each(function(){
+        var itemTop = card1.offset().top
+        if(documentTop > itemTop - 300) {
+            card1.addClass(animationClass)
         }
     })
 }
+
+function animeCard2(){
+    var documentTop = $(document).scrollTop()
+
+    card2.each(function(){
+        var itemTop = card2.offset().top
+        if(documentTop > itemTop - 300) {
+            card2.addClass(animationClass)
+        }
+    })
+}
+
+function animeCard3(){
+    var documentTop = $(document).scrollTop()
+
+    card3.each(function(){
+        var itemTop = card3.offset().top
+        if(documentTop > itemTop - 300) {
+            card3.addClass(animationClass)
+        }
+    })
+}
+
 animeItem();
 animeImg();
+
+$(document).scroll(function(){
+    animeAbout();
+    animeCard1();
+    animeCard2();
+    animeCard3();
+})
